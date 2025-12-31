9c55cfbe9941ee9fb4940476e53052cb12b4a64e
Claire <2420916021@qq.com>
2025-12-31 17:20:17 +0800

    品牌管理分页展示数据

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -31,11 +31,14 @@
         pagination
         ---v-model:current-page: 分页器当前页码
         ---v-model:page-size:设置每一个展示数据条数
-        ---:page-sizes: 设置每页展示条数可选项
+        ---:page-sizes: 设置每页展示条数可选项(下拉菜单)
         ---:background: 开启分页器背景色
         ---layout: 设置分页器六个子组件的布局
     -->
     <el-pagination
+      @size-change="sizeChange"
+      @current-change="getHasTrademark"
+      pager-count="9"
       v-model:current-page="pageNo"
       v-model:page-size="limit"
       :page-sizes="[3, 5, 7, 9]"
@@ -60,9 +63,13 @@ let total = ref<number>(0);
 // 存储已有品牌的数据
 let trademarkArr = ref<Records>([]);
 // 获取已有品牌的接口封装为一个函数：在任何情况下获取数据，调用此函数即可
-const getHasTrademark = async () => {
+const getHasTrademark = async (pager = 1) => {
+  // 当前页码
+  pageNo.value = pager;
+  console.log(pageNo.value,pager);
+  
   const res: TradeMarkResponseData = await reqHasTradeMark(pageNo.value, limit.value);
-  console.log(res);
+  // console.log(res);
   if (res.code === 200) {
     // 存储已有品牌的总个数
     total.value = res.data.total;
@@ -73,6 +80,21 @@ const getHasTrademark = async () => {
 onMounted(() => {
   getHasTrademark();
 });
+
+// 分页器当前页码发生变化的时候会触发
+// 对于当前页码发生变化自定义事件，组件pagination父组件回传了数据（当前的页码）
+// const changePageNo=(pager)=>{
+//   // 当前页码发生变化的时候再次发请求获取对应已有品牌数据展示
+//   getHasTrademark()
+// }
+
+// 当下拉菜单发生变化的时候会触发此方法
+// 这个自定义事件，分页器组件会将下拉菜单选中数据返回
+const sizeChange = () => {
+  // 当前每一页的数据量发生变化时，当前页码归1
+
+  getHasTrademark();
+};
 </script>
 
 <style scoped></style>
